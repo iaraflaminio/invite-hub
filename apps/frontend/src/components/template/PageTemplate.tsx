f33eb6770386cc84e22069f481df5b2c198d8411
Iara Esteves Flaminio <iaraflaminio@outlook.com>
2024-11-20 20:57:28 -0300

    Add Tailwind CSS and update PageTemplate component with initial styling

--- apps/frontend/src/components/template/PageTemplate.tsx
+++ apps/frontend/src/components/template/PageTemplate.tsx
@@ -5,7 +5,9 @@ export interface PageTemplateProps {
 
 export default function PageTemplate(props: PageTemplateProps) {
     return (
-        <div className={``}>
+        <div className="
+            flex flex-col items-center py-10
+        ">
             <main className={props.className}>
                 {props.children}
             </main>
