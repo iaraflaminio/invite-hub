4f187a356ccb41c6d94364001c54ee2d31c8e8fb
Iara Esteves Flaminio <iaraflaminio@outlook.com>
2024-11-21 17:20:05 -0300

    setting the font for logo span

--- apps/frontend/src/components/template/Logo.tsx
+++ apps/frontend/src/components/template/Logo.tsx
@@ -1,14 +1,15 @@
 import { Righteous } from 'next/font/google'
+import Link from 'next/link'
 
-const font = Righteous ({
+const font = Righteous({
     subsets: ['latin'],
     weight: "400",
 })
 
 export default function Logo() {
     return (
-        <div>
+        <Link href="/" className={` flex items-center gap-2 ${font.className} `}>
             <span>Logo</span>
-        </div>
+        </Link>
     )
 }
