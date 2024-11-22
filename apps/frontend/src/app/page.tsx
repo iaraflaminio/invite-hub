160686888b3ea7a4ad18b9fa87a84e32fbb9d352
Iara Esteves Flaminio <iaraflaminio@outlook.com>
2024-11-21 22:19:21 -0300

    importing Event component to app page

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,9 +1,11 @@
 import PageTemplate from "@/components/template/PageTemplate";
+import {Event} from "@/core";
 
 export default function Home() {
+  const e: Partial<Event> = {};
     return (
     <PageTemplate>
       <div>Home</div>
     </PageTemplate>
   )
-  }
+  };
