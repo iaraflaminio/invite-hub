ff4991c859f7e5696fdeacbcfaf5bdf0a6bd21a0
Iara Esteves Flaminio <iaraflaminio@outlook.com>
2024-11-22 18:27:49 -0300

    centralize imports and exports for guest and event utilities

--- apps/frontend/src/core/invite-hub/index.ts
+++ apps/frontend/src/core/invite-hub/index.ts
@@ -1,4 +1,15 @@
 import Event from "./model/Event";
 import Guest from "./model/Guest";
 
+import complementGuest from "./functions/complementGuest";
+import complementEvent from "./functions/complementEvent";
+import createEmptyEvent from "./functions/createEmptyEvent";
+import createEmptyGuest from "./functions/createEmptyGuest";
+
 export type { Event, Guest };
+export {
+  complementGuest,
+  complementEvent,
+  createEmptyEvent,
+  createEmptyGuest,
+}
