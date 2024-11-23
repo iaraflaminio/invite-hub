3000a40a35f103f8e161e9fc88838bda97a15d02
Iara Esteves Flaminio <iaraflaminio@outlook.com>
2024-11-23 17:25:08 -0300

    fixing: centering logo and logo text

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <div
       className="h-screen flex flex-col justify-center items-center gap-10 bg-[url('/background-inicio.svg')] bg-cover">
       
-      <div>
+      <div className="flex flex-col items-center">
         <Logo />
         <p className="text-zinc-500 font-light w-96 leading-6 text-center">
         Create and manage your event invitation quickly and easily!
