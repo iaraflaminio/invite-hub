2e16030694d2cc16f474ab36afc759cc3cffbda2
Iara Esteves Flaminio <iaraflaminio@outlook.com>
2024-11-22 23:08:15 -0300

    adding background image

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
+    <div
+      className="h-screen flex flex-col justify-center items-center gap-10 bg-[url('/background-inicio.svg')] bg-cover">
       <div>Home</div>
     </div>
   )
