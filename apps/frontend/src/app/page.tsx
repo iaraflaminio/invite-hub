9ddb3b22e56bd64fc2d9a902268478b60c8965cd
Iara Esteves Flaminio <iaraflaminio@outlook.com>
2024-11-22 23:19:29 -0300

    adding Link to create a new event

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Logo from "@/components/template/Logo";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
         Create and manage your event invitation quickly and easily!
         </p>
       </div>
+      <Link href="/event">Create your event</Link>
     </div>
   )
 };
